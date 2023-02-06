f3dc5cce9cd1b99652e8c024f2cf67294bd1cfd2
kishore0805 <124121535+kishore0805@users.noreply.github.com>
2023-02-06 08:27:29 +0530

    Create 31.shell program to swap two numbers.c

---
+++ 31.shell program to swap two numbers.c
@@ -0,0 +1,10 @@
+cho “ enter a first number”
+read first
+echo “ enter a  second number”
+read second
+temp=$first
+first=$second
+second=$temp
+  
+echo "After swapping, numbers are:"
+echo "first = $first, second = $second"
